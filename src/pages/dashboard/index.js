03fe9ef5593378ccfe690f258cca7d8715a5de83
AldianBaim <aldiansyahibrahim5@gmail.com>
2022-07-10 23:19:12 +0700

    Update beranda dashboard text

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,7 +4,7 @@ import Layout from '../../components/dashboard/Layout'
 const Home = () => {
     return (
         <Layout>
-            <div>Home</div>
+            <div>Beranda</div>
             <div className="my-3">
                 <img src="https://i.ibb.co/b6FkPKN/images-35.webp" className="w-100 img-fluid rounded" alt="Banner" />
 
