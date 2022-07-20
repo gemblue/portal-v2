d6287282ea6b189ccc91833f562382669457587e
Ahmad Oriza <ahmadoriza@gmail.com>
2022-07-20 07:47:21 +0700

    Build

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,2 +1,3 @@
 // API BASE URL
-export const BASE_URL = 'https://api.buku.kemdikbud.go.id';
+//export const BASE_URL = 'https://api.buku.kemdikbud.go.id';
+export const BASE_URL = 'https://api.buku.kemdikbud.cloudapp.web.id';
