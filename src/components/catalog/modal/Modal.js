00272ca20eea7c650a69a693e51751a97b132a5b
AldianBaim <aldiansyahibrahim5@gmail.com>
2023-03-27 15:57:42 +0700

    Fix style modal read padding

--- src/components/catalog/modal/Modal.js
+++ src/components/catalog/modal/Modal.js
@@ -3,7 +3,7 @@ const Modal = ({ children, title, id }) => {
         <div className="modal fade" id={id} tabIndex={-1} data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="ModalLabel" aria-hidden="true" style={{ width: '100%' }}>
             <div className="modal-dialog modal-xl">
                 <div className="modal-content">
-                    <div className="modal-header p-0 m-0 position-relative">
+                    <div className="modal-header p-0 m-0 border-0 position-relative">
                         <button type="button" className="btn-close position-absolute bg-light end-0" style={{zIndex: '100'}} data-bs-dismiss="modal" aria-label="Close" />
                     </div>
                     <div className="modal-body p-0 m-0">
