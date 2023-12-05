d76ceb23c5d4ef77937ed18fe44c86cc282c0c4f
AldianBaim <aldiansyahibrahim5@gmail.com>
2023-12-05 15:54:53 +0700

    Create tab chapter detail book

--- src/components/catalog/sections/SectionAudioPlayer/SectionAudioPlayer.js
+++ src/components/catalog/sections/SectionAudioPlayer/SectionAudioPlayer.js
@@ -1,62 +1,86 @@
-import React, { useState } from 'react'
-import ReactAudioPlayer from 'react-audio-player';
+import React, { useRef, useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
-const SectionAudioPlayer = ({ audio }) => {
+const SectionAudioPlayer = ({ audios }) => {
+  // Collect all chapter on book
+  const chapters = audios.filter(
+    (audio, index) =>
+      index === audios.findIndex((obj) => audio.chapter === obj.chapter)
+  );
 
-    const [limit, setLimit] = useState(10)
-    let number = 1
+  const [tabActive, setTabActive] = useState({
+    chapter: chapters[0].chapter,
+    button: 0,
+  });
+  const audioEl = useRef([]);
+  const [playedAudio, setPlayedAudio] = useState(null);
+  let number = 1;
 
-    const reset = () => {
-        setLimit(10);
-        setTimeout(() => {
-            document.getElementById('audioPlayer').scrollIntoView()
-        }, 0);
-    }
+  return (
+    <section id="audioPlayer">
+      <div className="container p-3">
+        <h4>Pemutar Audio</h4>
+        <div className="d-flex gap-2 my-3 overflow-auto">
+          {chapters.map((chapter, index) => {
+            return (
+              <button
+                className={`btn ${
+                  tabActive.button == index
+                    ? "btn-primary"
+                    : "btn-outline-primary"
+                }`}
+                onClick={() => {
+                  setTabActive({ chapter: chapter.chapter, button: index });
+                }}
+              >
+                Bab {chapter.chapter}
+              </button>
+            );
+          })}
+        </div>
+        <table className="table table-hover">
+          <thead className="bg-light">
+            <tr>
+              <th>NO</th>
+              <th>AUDIO</th>
+              <th>JUDUL</th>
+            </tr>
+          </thead>
+          <tbody>
+            {audios
+              .filter((audio) => audio.chapter == tabActive.chapter)
+              .map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td width="8%">{number++}</td>
+                    <td width="40%" className="p-0 pt-1">
+                      <ReactAudioPlayer
+                        className="w-100 bg-transparent"
+                        src={item.attachment}
+                        controls
+                        key={index}
+                        ref={(element) => (audioEl.current[index] = element)}
+                        onPlay={() => {
+                          setPlayedAudio((prev) => {
+                            if (!isNaN(prev) && index !== prev) {
+                              const audioElm =
+                                audioEl?.current[prev]?.audioEl?.current;
+                              audioElm?.pause();
+                            }
+                            return index;
+                          });
+                        }}
+                      />
+                    </td>
+                    <td>{item.title}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section id="audioPlayer">
-            <div className="container p-3">
-                <h4>Pemutar Audio</h4>
-                <table className='table table-hover'>
-                    <thead className="bg-light">
-                        <tr>
-                            <th>NO</th>
-                            <th>AUDIO</th>
-                            <th>JUDUL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            audio.map((item, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td width="8%">{number++}</td>
-                                        <td width="40%" className='p-0 pt-1'>
-                                            <ReactAudioPlayer
-                                                className='w-100 bg-transparent'
-                                                src={item.attachment} controls
-                                            />
-                                        </td>
-                                        <td>{item.title}</td>
-                                    </tr>
-
-                                )
-                            }).slice(0, limit)
-                        }
-                    </tbody>
-                </table>
-                <div className="text-center my-5">
-                    {
-                        audio.length >= limit && <button onClick={() => setLimit(limit + 5)} className="btn btn-primary">Load more</button>
-                    }
-                    {
-                        audio.length <= limit && <button onClick={() => reset()} className="btn btn-warning">Reset</button>
-                    }
-                </div>
-
-            </div>
-        </section >
-    )
-}
-
-export default SectionAudioPlayer
+export default SectionAudioPlayer;
